ff3b82d11c9652b108afcabceb78904089f59061
WilbertBocanegra <wilbertbocanegra@gmail.com>
2021-07-28 14:37:10 -0500

    first commit

--- main.ts
+++ main.ts
@@ -8,6 +8,8 @@ const app: express.Application = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/", ExampleRouter);
 
 app.listen(config.PORT, config.HOST, () => {
